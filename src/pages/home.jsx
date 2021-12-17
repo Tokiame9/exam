7d87aec0c35690ef3e6799f6ee86d7bb9b4d9737
yonaka99 <825269487@qq.com>
2021-12-17 11:08:19 +0800

    创建

---
+++ src/pages/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function home() {
+    return (
+        <div>
+            <div className='prepare'>
+
+            </div>
+            <div className='doing'>
+                
+            </div>
+            <div className='complete'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default home
---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function login() {
+    return (
+        <div>
+            <button className="btnStart">start APP</button>
+        </div>
+    )
+}
+
+export default login
